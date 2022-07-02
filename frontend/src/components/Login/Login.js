b499dcc830667e51c6696789ae4e103b4fa71b08
Da-vilu <danielvinagre98@gmail.com>
2022-07-02 11:26:49 +0200

    added css

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -40,58 +40,74 @@ const Login = () => {
         setError(body.message);
       } else {
         setToken(body.data.token);
+        return navigate('/articles');
       }
     } catch (err) {
       console.error(err);
       setError(err.message);
     } finally {
       setLoading(false);
-      return navigate('/articles');
     }
   };
-
   return (
-    <div className='form'>
-      <div className='form-panel'>
-        <div className='form-header'>
-          <h1>Account Login</h1>
-        </div>
-        <div className='form-content'>
-          <form>
-            <div className='form-group'>
-              <label htmlFor='username'>Email</label>
-              <input
-                type='email'
-                name='email'
-                value={email || ''}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='password'>Password</label>
-              <input
-                type='password'
-                name='pass'
-                value={password || ''}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div className='form-group'>
-                <p className='form-singup'>
+    <div className='center'>
+      <div className='container'>
+        <div className='row'>
+          <div
+            className='col-10 offset-1 col-sm-6 offset-sm-0 offset-sm-3
+           col-lg-4 offset-lg-0 offset-lg-4 col-xl-4 offset-xl-0 offset-xl-4
+             border shadow p-3 mb-5 bg-body rounded'
+          >
+            <h1>Log In</h1>
+            <form>
+              <div className='mb-3'>
+                <label htmlFor='email' className='form-label'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  className='form-control'
+                  placeholder='Enter your email'
+                  value={email || ''}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='pass'
+                  className='form-control'
+                  value={password || ''}
+                  placeholder='Enter your password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <p>
                   Not registered yet? -
                   <NavLink to='/users' className='loginLink'>
                     click here
                   </NavLink>
                 </p>
               </div>
-            </div>
-            <div disabled={loading} className='form-group'>
-              <button type='submit' onClick={handleSubmit}>
-                Log In
-              </button>
-            </div>
-          </form>
+
+              <div disabled={loading} className='d-grid gap-2 col-6 mx-auto'>
+                <button
+                  type='submit'
+                  onClick={handleSubmit}
+                  className='btn btn-primary'
+                >
+                  Log In
+                </button>
+              </div>
+              {error ? <p className='error'>{error}</p> : null}
+            </form>
+          </div>
         </div>
-        {error && <p className='Error'>{error}</p>}
       </div>
     </div>
   );
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,10 +1,9 @@
+import './Register.css';
 import { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useToken } from '../../context/TokenContext';
 
-import './Register.css';
-
-const Register = () => {
+export const Register = () => {
   let navigate = useNavigate();
   const [token] = useToken();
   const [alias, setAlias] = useState('');
@@ -13,12 +12,15 @@ const Register = () => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [rPass, setRpass] = useState('');
   const [biography, setBiography] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
 
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState(null);
   const [error, setError] = useState(null);
+  // Si estamos logueados redireccionamos a la página principal.
+  // if (token) return <Navigate to="/" />;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,6 +29,9 @@ const Register = () => {
     setLoading(true);
 
     try {
+      if (password !== rPass) {
+        return setError("Passwords don't match");
+      }
       // Si queremos enviar un body con formato "form/data" es necesario
       // crear un objeto de tipo FormData y "pushear" los elementos que queramos
       // enviar.
@@ -62,12 +67,17 @@ const Register = () => {
     }
   };
 
+  const handleBack = (e) => {
+    e.preventDefault();
+    return navigate('/login');
+  };
+
   useEffect(() => {
-    const successP = document.querySelector('p.Success');
+    const successP = document.querySelector('p.success');
 
     if (successP) {
       const t = setTimeout(() => {
-        document.querySelector('p.Success').remove();
+        document.querySelector('p.success').remove();
         return navigate('/login');
       }, 3000);
 
@@ -76,82 +86,149 @@ const Register = () => {
   });
 
   return (
-    <main className='Register'>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='alias'>Alias:</label>
-        <input
-          type='text'
-          name='alias'
-          value={alias || ''}
-          onChange={(e) => setAlias(e.target.value)}
-        />
-
-        <label htmlFor='name'>Name:</label>
-        <input
-          type='text'
-          name='name'
-          value={name || ''}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <label htmlFor='firstName'>First name:</label>
-        <input
-          type='text'
-          name='firstName'
-          value={firstName || ''}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-
-        <label htmlFor='lastName'>Last name:</label>
-        <input
-          type='text'
-          name='lastName'
-          value={lastName || ''}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-
-        <label htmlFor='email'>Email:</label>
-        <input
-          type='email'
-          name='email'
-          value={email || ''}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <label htmlFor='pass'>Contraseña:</label>
-        <input
-          type='password'
-          name='pass'
-          value={password || ''}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <label htmlFor='biography'>Biography:</label>
-        <textarea
-          type='text'
-          name='biography'
-          value={biography || ''}
-          onChange={(e) => setBiography(e.target.value)}
-        />
-
-        <label htmlFor='image'>Avatar:</label>
-        <input
-          type='file'
-          onChange={(e) => {
-            setSelectedFile(e.target.files[0]);
-          }}
-        />
-
-        <div className='Button'>
-          <NavLink to='/login'>Back</NavLink>
+    <div className='center'>
+      <div className='container'>
+        <div className='row'>
+          <div
+            className='col-10 offset-1 col-sm-6 offset-sm-0 offset-sm-3
+           col-lg-4 offset-lg-0 offset-lg-4 col-xl-4 offset-xl-0 offset-xl-4
+             border shadow p-3 mb-5 bg-body rounded'
+          >
+            <h1 className='text-primary'>Register</h1>
+            <form onSubmit={handleSubmit}>
+              <div className='mb-3'>
+                <label htmlFor='alias'>Alias</label>
+                <input
+                  type='text'
+                  name='alias'
+                  className='form-control'
+                  value={alias || ''}
+                  placeholder='Enter your alias'
+                  onChange={(e) => setAlias(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='name'>Name</label>
+                <input
+                  type='text'
+                  name='name'
+                  className='form-control'
+                  value={name || ''}
+                  placeholder='Enter your name'
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='firstName'>First name</label>
+                <input
+                  type='text'
+                  name='firstName'
+                  className='form-control'
+                  value={firstName || ''}
+                  placeholder='Enter your first name'
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='lastName'>Last name</label>
+                <input
+                  type='text'
+                  name='lastName'
+                  className='form-control'
+                  value={lastName || ''}
+                  placeholder='Enter your last name'
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='email' className='form-label'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  className='form-control'
+                  value={email || ''}
+                  placeholder='Enter your email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='pass'
+                  className='form-control'
+                  value={password || ''}
+                  placeholder='Enter your password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='rPass' className='form-label'>
+                  Repeat Password
+                </label>
+                <input
+                  type='password'
+                  name='rPass'
+                  className='form-control'
+                  value={rPass || ''}
+                  placeholder='Re-enter your password'
+                  onChange={(e) => setRpass(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='biography'>Biography</label>
+                <textarea
+                  rows='4'
+                  cols='50'
+                  maxLength='210'
+                  type='text'
+                  name='biography'
+                  className='form-control'
+                  value={biography || ''}
+                  onChange={(e) => setBiography(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <input
+                  type='file'
+                  onChange={(e) => {
+                    setSelectedFile(e.target.files[0]);
+                  }}
+                />
+              </div>
+              <div className='row'>
+                <div className='col-6'>
+                  <div className='mb-3'>
+                    <button
+                      className='btn btn-danger col-12'
+                      onClick={handleBack}
+                    >
+                      Back
+                    </button>
+                  </div>
+                </div>
+                <div className='col-6'>
+                  <div className='mb-3'>
+                    <button
+                      className='btn btn-primary col-12'
+                      disabled={loading}
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+            {error && <p className='error'>{error}</p>}
+            {message && <p className='success'>{message}</p>}
+          </div>
         </div>
-        <button className='submit' disabled={loading}>
-          Sign Up
-        </button>
-      </form>
-      {error && <p className='Error'>{error}</p>}
-      {message && <p className='Success'>{message}</p>}
-    </main>
+      </div>
+    </div>
   );
 };
 
